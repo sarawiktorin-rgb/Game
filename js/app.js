ba4fab09073c96411c2db73c64cfef20508351d0
sarawiktorin-rgb <sara.wiktorin@gmail.com>
2026-04-15 13:47:44 +0200

    button + score display

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+// Click a button or object to earn points so that I can increase my score.
+// See my current score during the game so that I know how well I am doing.
+// See a countdown timer so that I know how much time is left.
+
+// Variables
+let score = 0;
+
+// HTML DOM
+const button1 = document.getElementById('button1');
+const scoreDisplay = document.getElementById('scoreDisplay');
+
+// UI Functions
+button1.addEventListener('click', () => {
+  increaseScore();
+})
+
+// Functions
+function increaseScore() {
+  score++;
+  scoreDisplay.innerText = score;
+}
